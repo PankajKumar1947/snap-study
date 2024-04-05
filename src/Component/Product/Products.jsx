7ba13b12ba3afc59c6f3907525025dabc3fd8323
Priyanshu Paul <priyanshupaul32@gmail.com>
2024-04-05 21:51:13 +0530

    updated the hero section along with product section

--- src/Component/Product/Products.jsx
+++ src/Component/Product/Products.jsx
@@ -11,18 +11,18 @@ const Products = () => {
         },
         {
             route:"playlist",
-            title:"Chapterwise Youtube Playlist",
-            desc:"Get all the top most lecture of Your Subject which will help you to boost your prepartion."
+            title:"Chapterwise Video  Playlist",
+            desc:"Don't worry here you will get all the video lecture  of each chapter in one playlist."
         },
        {
             route:"organiser",
             title:"Get the latest Orgniser",
-            desc:"Get all the top most lecture of Your Subject which will help you to boost your prepartion."
+            desc:"This section is for latest organisers, 1 stop solution  for all organizer needs."
         },
         {
             route:"pyq",
             title:"Yearwise Subject PYQ",
-            desc:"Get all the top most lecture of Your Subject which will help you to boost your prepartion."
+            desc:"Boost your preparation with yearwise  subject PYQ. Get it now!"
         },
     ]
 
@@ -33,8 +33,8 @@ const Products = () => {
     <div className='text-white grid md:grid-cols-2 bg-gradient-to-r from-gray-600 to-gray-900 justify-between items-center py-4 px-2 sm:px-8 md:py-14  '>
         {/* left */}
         <div className='sm:p-4 text-center'>
-            <h1 className='text-2xl md:text-4xl text-center my-1 font-bold'>Our Products</h1>
-            <p className='text-center p-2 sm:p-5 text-lg font-thin'> ​This is the space to introduce the Product section and showcase the types of products available. </p>
+            <h1 className='text-2xl md:text-4xl text-center my-1 font-bold'>Main highlights </h1>
+            <p className='text-center p-2 sm:p-5 text-lg font-thin'> Hop on and explore our main features here!</p>
             <button className="hidden sm:block mx-auto px-3 py-2 sm:px-6 sm:py-3 rounded-full text-gray-800 bg-white hover:bg-gray-400 duration-200">Read More</button>
             
         </div>
