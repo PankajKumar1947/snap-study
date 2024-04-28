759fadb6f0d1035009d337d0371710e752b61e11
Pankaj Kumar <pankaj.ky3007@gmail.com>
2024-04-28 14:06:56 +0530

    uploading soon text- size

--- src/Component/Error/UploadedSoon.jsx
+++ src/Component/Error/UploadedSoon.jsx
@@ -4,18 +4,18 @@ import { NavLink } from 'react-router-dom'
 const UploadedSoon = () => {
   return (
     <div className="text-center w-full h-[40vh] flex flex-col items-center justify-center mx-auto px-2 lg:px-8 z-20">
-        <h2 className="text-3xl font-extrabold  text-white sm:text-4xl">
+        <h2 className="text-xl font-extrabold  text-white sm:text-4xl">
             <span className="block">
               Uploading Soon!
             </span>
         </h2>
-        <p className="text-xl mt-4 max-w-md mx-auto ">
+        <p className="text-normal sm:text-xl mt-4 max-w-md mx-auto ">
             This page is not available right now, but it will be soon. Stay tuned!
         </p>
         <div className='mt-5'>
             <NavLink 
             to={"/"}
-            className="bg-white text-black px-5 py-3 rounded-full font-semibold ">
+            className="bg-white text-black px-3 py-2 sm:px-5 sm:py-3  rounded-full font-semibold ">
                 <button className=''>Back to Home</button>
             </NavLink>
         </div>
