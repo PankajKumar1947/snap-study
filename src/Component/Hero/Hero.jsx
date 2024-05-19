43d24bfe80665ae7da5a7c1c9b10a604ad702b7e
Pankaj Kumar <pankaj.ky3007@gmail.com>
2024-05-19 16:14:52 +0530

    meta tag added

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -23,8 +23,8 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black opacity-50"></div>
 
       <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
-        <h2 className='text-xs sm:text-2xl my-2 text-center'>Explore latest makaut study material</h2>
-        <h1 className="text-xl sm:text-2xl md:text-4xl lg:text-6xl font-bold mb-4">Organised with ease </h1>
+        <h1 className='text-xs sm:text-2xl my-2 text-center'>Explore latest makaut study material</h1>
+        <h2 className="text-xl sm:text-2xl md:text-4xl lg:text-6xl font-bold mb-4">Organised with ease </h2>
         <button 
         onClick={readnowHandler}
         className="bg-gray-800 text-white px-3 py-2 sm:px-6 sm:py-3 text-xs sm:text-lg rounded-full hover:text-gray-800 hover:bg-white duration-200">Read Now</button>
