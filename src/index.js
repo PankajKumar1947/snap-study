544a6bf8ae57e8067a4475c8a990642d8c35a572
Pankaj Kumar <pankaj.ky3007@gmail.com>
2024-05-19 15:04:43 +0530

    speed insights added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import App from "./App";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react";
 import { HelmetProvider } from 'react-helmet-async';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
